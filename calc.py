d739e80c08d90842920d0b34d31660c6f8c0eb39
Sac6567 <sac6567@gmail.com>
2022-09-20 15:11:27 +0530

    Created Display Labels

--- calc.py
+++ calc.py
@@ -18,9 +18,24 @@ class Calculator:
         self.window.resizable(0,0)
         self.window.title("Calculator")
 
+        self.total_expression = "0"
+        self.current_expression = "0"
+
         self.display_frame = self.create_display_frame()
+
+        self.total_label, self.label = self.create_display_labels()
         self.buttons_frame = self.create_buttons_frame()
 
+    def create_display_labels(self):
+        total_label = tk.Label(self.display_frame, text=self.total_expression, anchor=tk.E, bg=LIGHT_GRAY,
+                               fg=LABEL_COLOR, padx=24, font=SMALL_FONT_STYLE)
+        total_label.pack(expand=True, fill='both')
+
+        label = tk.Label(self.display_frame, text=self.current_expression, anchor=tk.E, bg=LIGHT_GRAY,
+                               fg=LABEL_COLOR, padx=24, font=LARGE_FONT_STYLE)
+        label.pack(expand=True, fill='both')
+
+        return  total_label,label
     def create_display_frame(self):
         frame = tk.Frame(self.window, height=221, bg=LIGHT_GRAY)
         frame.pack(expand=True, fill="both")
